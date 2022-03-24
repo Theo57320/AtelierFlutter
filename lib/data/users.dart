84c6b089a6a4e491c11fcfd710f69e87360bf057
Thibault-Am <amagat.thibault@gmail.com>
2022-03-24 14:41:58 +0100

    tiens theo

--- lib/data/users.dart
+++ lib/data/users.dart
@@ -5,22 +5,3 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:reunionou/models/user.dart';
 import 'package:http/http.dart' as http;
-
-Future<User> fetchUser(http.Client client) async {
-  var _authUri = 'http://docketu.iutnc.univ-lorraine.fr:62360/auth';
-  String basicAuth ='Basic ' + base64Encode(utf8.encode('$email:$password'));
-  var response = await Dio().get(
-    _authUri,
-    options: Options(
-      headers: <String, String>{'authorization': basicAuth}
-    ),
-  );
-  print(response.statusCode);
-  return compute(parseUser, response.data.toString());
-}
-
-List<User> parseUser(String responseBody) {
-  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-
-  return parsed.map<User>((json) => User.fromJson(json)).toList();
-}
--- lib/data/users_collection.dart
+++ lib/data/users_collection.dart
@@ -8,16 +8,5 @@ import 'package:reunionou/models/user.dart';
 import 'package:reunionou/screens/event_map.dart';
 
 class UserCollection extends ChangeNotifier {
-  var connecte =false ;
-  var data_user=[];
-  void connecte() {
-    connecte = !connecte;
-    // This call tells the widgets that are listening to this model to rebuild.
-    notifyListeners();
-  }
-  void logout(User? user) {
-    selectedUser = null;
-    // This call tells the widgets that are listening to this model to rebuild.
-    notifyListeners();
-  }
+  String title = 'Reunionou';
 }
